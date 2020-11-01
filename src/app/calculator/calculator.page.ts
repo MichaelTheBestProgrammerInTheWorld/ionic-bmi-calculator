0183f688bffcb29cb3b95ef23908e249ec2ee877
Michael <michaelmagdy_93@yahoo.com>
2020-11-01 15:42:21 +0200

    init commit

---
+++ src/app/calculator/calculator.page.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.page.html',
+  styleUrls: ['./calculator.page.scss'],
+})
+export class CalculatorPage implements OnInit {
+
+  height: number;
+  weight: number;
+
+  bmiValue: number;
+  bmiMessage: string;
+
+  calculateBMI() {
+    // Add logic to calculate BMI here
+    if (this.weight > 0 && this.height > 0) {
+      let finalBmi = this.weight / (this.height / 100 * this.height / 100);
+      this.bmiValue = parseFloat(finalBmi.toFixed(2));
+      this.setBMIMessage();
+    }
+  }
+
+  private setBMIMessage() {
+    if (this.bmiValue < 18.5) {
+      this.bmiMessage = "Underweight"
+    }
+  
+    if (this.bmiValue > 18.5 && this.bmiValue < 25) {
+      this.bmiMessage = "Normal"
+    }
+  
+    if (this.bmiValue > 25 && this.bmiValue < 30) {
+      this.bmiMessage = "Overweight"
+    }
+  
+    if (this.bmiValue > 30) {
+      this.bmiMessage = "Obese"
+    }
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
